bb4b1340b511e440926de55f6c90d844353cfc7a
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2022-09-30 18:40:06 +0200

    updating styles

--- src/app/doctor/components/students/students.component.ts
+++ src/app/doctor/components/students/students.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns :any ;
+  dataSource :any ;
+  datatable:any
+  constructor() {
+    this.displayedColumns =["position", "name" ,"subjectName" , "degree"];
+    
+   }
 
   ngOnInit(): void {
   }
